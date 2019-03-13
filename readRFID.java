e465b5cbea8c9f603279f83214138004454ab7ce
zaki <zihali91@gmail.com>
2019-03-13 11:08:37 +0000

    reversion to original code now got array of serial nums to work

--- readRFID.java
+++ readRFID.java
@@ -1,13 +1,14 @@
-
 import com.phidgets.*;
 import com.phidgets.event.*;
 
 public class readRFID {
-	public String theRFIDNumber;
+static //	public String theRFIDNumber;
 	RFIDPhidget rfid;
 	
-	public readRFID() throws Exception {
-            rfid = new RFIDPhidget();
+	public readRFID(int theRFIDSerial) throws Exception {
+		//RFIDPhidget rfid;
+		rfid = new RFIDPhidget();
+		//add attach listener
 		rfid.addAttachListener(new AttachListener() {
 			public void attached(AttachEvent ae)
 			{
@@ -17,7 +18,6 @@ public class readRFID {
 					((RFIDPhidget)ae.getSource()).setLEDOn(true);
 				}
 				catch (PhidgetException ex) { }
-				System.out.println("attachment of " + ae);
 			}
 		});
 		rfid.addDetachListener(new DetachListener() {
@@ -30,51 +30,49 @@ public class readRFID {
 				System.out.println("error event for " + ee);
 			}
 		});
-		rfid.addTagGainListener(new TagGainListener()
-		{
-			public void tagGained(TagGainEvent oe)
+		
+		rfid.open(theRFIDSerial);
+		rfid.waitForAttachment();
+		System.out.println("working");
+	}
+	public readRFID() throws Exception {
+		//RFIDPhidget rfid;
+		rfid = new RFIDPhidget();
+		//add attach listener
+		rfid.addAttachListener(new AttachListener() {
+			public void attached(AttachEvent ae)
 			{
-				System.out.println("Tag Gained: " +oe.getValue() + " (Proto:"+ oe.getProtocol()+")");
+				try
+				{
+					((RFIDPhidget)ae.getSource()).setAntennaOn(true);
+					((RFIDPhidget)ae.getSource()).setLEDOn(true);
+				}
+				catch (PhidgetException ex) { }
 			}
 		});
-		rfid.addTagLossListener(new TagLossListener()
-		{
-			public void tagLost(TagLossEvent oe)
-			{
-				System.out.println(oe);
+		rfid.addDetachListener(new DetachListener() {
+			public void detached(DetachEvent ae) {
+				System.out.println("detachment of " + ae);
 			}
 		});
-		rfid.addOutputChangeListener(new OutputChangeListener()
-		{
-			public void outputChanged(OutputChangeEvent oe)
-			{
-				System.out.println(oe);
+		rfid.addErrorListener(new ErrorListener() {
+			public void error(ErrorEvent ee) {
+				System.out.println("error event for " + ee);
 			}
 		});
-
 		rfid.openAny();
-		System.out.println("waiting for RFID attachment...");
-		rfid.waitForAttachment(10000);
-		
-		while (true) {
-			boolean tagGained = rfid.getTagStatus();
-			if (tagGained = true) {
-				
-			}
-		}
+		rfid.waitForAttachment(1000);
+		System.out.println("working");
 	}
-
-	public boolean tagPresent(boolean s) throws PhidgetException {
-		return s = rfid.getTagStatus();
-	}
-	public String getTag() {
-		
+	
+	public static String getTag() throws Exception {
 		String tagString = "";
-		try {
-			tagString = rfid.getLastTag();
-		} catch (PhidgetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if(rfid.getTagStatus()) {
+			try {
+				tagString = rfid.getLastTag();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		return tagString;
 	}
